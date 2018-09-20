2fcbba2f9d8bee8ff484eb64fe2121e0b28fb56e
young <qichuanyang@hotmail.com>
2018-09-20 10:25:58 +0800

    tune

--- single_dialog.py
+++ single_dialog.py
@@ -23,7 +23,7 @@ tf.flags.DEFINE_integer("epochs", 200, "Number of epochs to train for.")
 tf.flags.DEFINE_integer("embedding_size", 20,
                         "Embedding size for embedding matrices.")
 tf.flags.DEFINE_integer("memory_size", 50, "Maximum size of memory.")
-tf.flags.DEFINE_integer("task_id", 6, "bAbI task id, 1 <= id <= 6")
+tf.flags.DEFINE_integer("task_id", 1, "bAbI task id, 1 <= id <= 6")
 tf.flags.DEFINE_integer("random_state", None, "Random state.")
 tf.flags.DEFINE_string("data_dir", "data/dialog-bAbI-tasks/",
                        "Directory containing bAbI tasks")
@@ -31,7 +31,7 @@ tf.flags.DEFINE_string("model_dir", "model/",
                        "Directory containing memn2n model checkpoints")
 tf.flags.DEFINE_boolean('train', True, 'if True, begin to train')
 tf.flags.DEFINE_boolean('interactive', False, 'if True, interactive')
-tf.flags.DEFINE_boolean('OOV', False, 'if True, use OOV test set')
+tf.flags.DEFINE_boolean('OOV', True, 'if True, use OOV test set')
 FLAGS = tf.flags.FLAGS
 print("Started Task:", FLAGS.task_id)
 
