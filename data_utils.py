5e136267b32236a99c2bc401e3e98473f3907cc2
young <qichuanyang@hotmail.com>
2018-09-29 15:22:27 +0800

    tune

--- data_utils.py
+++ data_utils.py
@@ -170,6 +170,7 @@ def vectorize_candidates(candidates,word_idx,sentence_size):
     for i,candidate in enumerate(candidates):
         lc=max(0,sentence_size-len(candidate))
         C.append([word_idx[w] if w in word_idx else 0 for w in candidate] + [0] * lc)
+    pdb.set_trace()
     return tf.constant(C,shape=shape)
 
 
@@ -189,10 +190,10 @@ def vectorize_data(data, word_idx, sentence_size, batch_size, candidates_size, m
     A = []
     tags_data = []
     data.sort(key=lambda x:len(x[0]),reverse=True)
-    # memory_size=max_memory_size
+    memory_size=max_memory_size
     for i, (story, query, answer) in enumerate(data):
-        if i%batch_size==0:
-            memory_size=max(1,min(max_memory_size,len(story)))
+        # if i%batch_size==0:
+        #     memory_size=max(1,min(max_memory_size,len(story)))
         ss = []
         for i, sentence in enumerate(story, 1):
             ls = max(0, sentence_size - len(sentence))
