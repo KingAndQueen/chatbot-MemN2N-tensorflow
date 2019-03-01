71668fa3ea334ca6fabfe3be7e3ecf5ffb83383b
young <qichuanyang@hotmail.com>
2019-03-01 12:57:42 +0800

    add trained embedding

--- memn2n/memn2n_dialog.py
+++ memn2n/memn2n_dialog.py
@@ -204,6 +204,7 @@ class MemN2NDialog(object):
             nil_word_slot = tf.zeros([1, self._embedding_size])
 
             if self._my_embedding is not None:
+                self._my_embedding = tf.concat([nil_word_slot, self._my_embedding], 0)
                 initial=tf.constant_initializer(value=self._my_embedding,dtype=tf.float32)
                 # pdb.set_trace()
                 # trained_emb=tf.get_variable('embedding_word', shape=[self._vocab_size, self._embedding_size],
