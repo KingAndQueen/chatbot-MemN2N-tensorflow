57f67fa0679702cbe1e180268f6b2a6ea04c3759
young <qichuanyang@hotmail.com>
2019-03-01 12:33:57 +0800

    add trained embedding

--- memn2n/memn2n_dialog.py
+++ memn2n/memn2n_dialog.py
@@ -213,7 +213,7 @@ class MemN2NDialog(object):
                 self.H = tf.Variable(self._init(
                     [self._embedding_size, self._embedding_size]), name="H")
                 # W = tf.concat([nil_word_slot, tf.get_variable('trained_embedding_W', shape=[self._vocab_size, self._embedding_size],initializer=initial, trainable=True)], 0)
-                self.W = tf.get_variable(name="W",shape=[self._vocab_size, self._embedding_size],initializer=initial)K
+                self.W = tf.get_variable(name="W",shape=[self._vocab_size, self._embedding_size],initializer=initial)
             else:
                 A = tf.concat([nil_word_slot, self._init(
                     [self._vocab_size - 1, self._embedding_size])], 0)
